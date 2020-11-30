0b9957aacda81f14b0faf1f68ce7b9131f2bf870
akhilponnala <58470902+akhilponnala@users.noreply.github.com>
2020-11-30 19:24:59 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,10 @@
+package com.akhiltest.analytics.proto.model.messages.*;
 public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("I am your awesome Java application!");
+        System.out.println("I am your awesome Java application!"+com.akhiltest.analytics.proto.model.messages.AuditDataMsg.getDescriptor();
+);
 
         DeepThought ourSuperComputer = new DeepThought();
         int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
